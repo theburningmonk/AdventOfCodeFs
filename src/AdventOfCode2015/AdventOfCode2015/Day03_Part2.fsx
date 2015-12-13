3eccd117a321c5e4dcd5eb9af7a2595dba7f2f58
theburningmonk <theburningmonk@gmail.com>
2015-12-13 13:16:50 +0000

    - added Day 3 part 2

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day03_Part2.fsx
@@ -0,0 +1,18 @@
+﻿#load "Day03.fs"
+
+open Day03
+
+let move ((x, y)::_ as acc) dir =
+    match dir with
+    | '^' -> (x, y-1)::acc
+    | '>' -> (x+1, y)::acc
+    | 'v' -> (x, y+1)::acc
+    | '<' -> (x-1, y)::acc
+
+input
+|> Seq.mapi (fun i x -> i%2, x)
+|> Seq.groupBy fst
+|> Seq.map (fun (_, gr) -> gr |> Seq.map snd)
+|> Seq.collect (Seq.fold move [(0, 0)])
+|> Seq.distinct
+|> Seq.length
