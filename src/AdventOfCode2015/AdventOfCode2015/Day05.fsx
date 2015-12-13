233b171ee00cb21f0cfe959f3eaac5ad07bdafb0
Yan Cui <theburningmonk@gmail.com>
2015-12-13 02:04:55 +0000

    - added Day 5

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day05.fsx
@@ -0,0 +1,31 @@
+﻿#load "Day05.fs"
+
+open Day05
+
+let (<&&>) f g x = f x && g x
+
+let atLeast3Vowels (word : string) =
+    word
+    |> Seq.filter (function 
+        | 'a' | 'e' | 'i' | 'o' | 'u' -> true 
+        | _ -> false)
+    |> Seq.length
+    |> (fun n -> n >= 3)
+
+let atLeast1Double (word : string) =
+    word
+    |> Seq.windowed 2
+    |> Seq.exists (fun [| a; b |] -> a = b)
+
+let noThese lst (word : string) =
+    lst 
+    |> List.exists word.Contains
+    |> not
+
+input.Split('\n')
+|> Array.map (fun x -> x.Trim())
+|> Array.filter 
+    (atLeast3Vowels <&&> 
+     atLeast1Double <&&> 
+     noThese [ "ab"; "cd"; "pq"; "xy" ])
+|> Array.length
