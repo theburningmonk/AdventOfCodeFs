e1d3ea39e98070300ee79ef6018b2adcacace67c
Yan Cui <theburningmonk@gmail.com>
2015-12-13 01:52:58 +0000

    - added Day 4

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day04.fsx
@@ -0,0 +1,19 @@
+﻿#r "System.Security"
+
+let md5 = System.Security.Cryptography.MD5.Create()
+
+Seq.unfold (fun n -> Some (n+1, n+1)) 0
+|> Seq.map (fun n ->
+    let hash = 
+        sprintf "bgvyzdsv%d" n
+        |> System.Text.Encoding.UTF8.GetBytes
+        |> md5.ComputeHash
+    n, hash)
+|> Seq.filter (fun (_, bin) ->
+    // take advantage of binary-to-hex conversion (4 bits = 1 hex decimal)
+    // first 5 hexdecimal = 0 means 
+    //      first 2 bytes == 0uy
+    //      3rd byte < 16uy
+    bin.[0] = 0uy && bin.[1] = 0uy && bin.[2] < 16uy)
+|> Seq.head
+|> fst
