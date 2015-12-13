e745ad026f44b4c683dcaa07e302f6c520e4a76f
Yan Cui <theburningmonk@gmail.com>
2015-12-13 01:10:13 +0000

    - added Day 1 and Day 2

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day01.fsx
@@ -0,0 +1,6 @@
+﻿#load "Day01.fs"
+
+open Day01
+
+input
+|> Seq.fold (fun n x -> match x with | '(' -> n+1 | _ -> n-1) 0
---
+++ src/AdventOfCode2015/AdventOfCode2015/Day02.fsx
@@ -0,0 +1,15 @@
+﻿#load "Day02.fs"
+
+open System
+open Day02
+
+let split (by : char) (x : string) = 
+    x.Split([| by |], StringSplitOptions.RemoveEmptyEntries)
+
+input
+|> split '\n'
+|> Array.map (fun l -> l |> split 'x' |> Array.map int)
+|> Array.sumBy (fun [| l; w; h |] ->
+    let area  = 2*l*w + 2*w*h + 2*h*l
+    let extra = [ l*w; w*h; l*h ] |> List.min
+    area + extra)
