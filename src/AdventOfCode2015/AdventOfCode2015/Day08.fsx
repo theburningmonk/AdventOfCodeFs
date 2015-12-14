f56f0dced896a42421cbb9c926e63f3b98e27b07
Yan Cui <theburningmonk@gmail.com>
2015-12-14 18:34:18 +0000

    - added Day 8 and Day 8 Part 2

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day08.fsx
@@ -0,0 +1,28 @@
+﻿open System.IO
+
+let input = 
+    __SOURCE_DIRECTORY__ + "\Day08_Input.txt"
+    |> File.ReadAllLines
+
+let (|IsEscaped|_|) = function
+    | '\\' :: '\\' :: rest -> Some ('\\', rest)
+    | '\\' :: '\"' :: rest -> Some ('\"', rest)
+    | '\\' :: 'x' :: x :: y :: rest ->
+        let char = sprintf "0x%c%c" x y |> int |> char
+        Some (char, rest)
+    | _ -> None
+
+let read (input : string) =
+    // remove leading and trailing "
+    let chars = input.Trim('\"') |> Seq.toList
+    
+    let rec read acc = function
+        | IsEscaped (char, rest) -> read (char::acc) rest
+        | hd::rest -> read (hd::acc) rest
+        | [] -> acc
+
+    read [] chars
+
+let codeSize = input |> Array.sumBy Seq.length
+let memSize  = input |> Array.sumBy (read >> Seq.length)
+let answer   = codeSize - memSize
---
+++ src/AdventOfCode2015/AdventOfCode2015/Day08_Part2.fsx
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+
+let input = 
+    __SOURCE_DIRECTORY__ + "\Day08_Input.txt"
+    |> File.ReadAllLines
+
+let encode (input : string) =
+    let rec encode acc = function
+        | '\"' :: rest -> encode ('\\'::'\"'::acc) rest
+        | '\\' :: rest -> encode ('\\'::'\\'::acc) rest
+        | hd :: rest   -> encode (hd::acc) rest
+        | [] -> acc
+    
+    [ 
+        yield '\"'
+        yield! input |> Seq.toList |> encode []
+        yield '\"'
+    ]
+
+let codeSize = input |> Array.sumBy Seq.length
+let memSize  = input |> Array.sumBy (encode >> Seq.length)
+let answer   = memSize - codeSize
