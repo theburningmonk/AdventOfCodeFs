0317df8f7bc82d0673ffb98255e5931e28f216b3
Yan Cui <theburningmonk@gmail.com>
2015-12-15 00:21:43 +0000

    - added Day 10 part 1 and 2

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day10_Part2.fsx
@@ -0,0 +1,17 @@
+﻿let input = "3113322113"
+
+let read (input : string) =
+    input
+    |> Seq.fold (fun acc x ->
+        match acc with
+        | (n, x')::tl when x = x' ->
+            (n+1, x')::tl
+        | _ -> (1, x)::acc) []
+    |> List.rev
+    |> Seq.collect (fun (n, x) ->
+        sprintf "%d%c" n x)
+    |> fun xs -> System.String.Join("", xs)
+
+{ 1..50 }
+|> Seq.fold (fun last _ -> read last) input
+|> Seq.length
