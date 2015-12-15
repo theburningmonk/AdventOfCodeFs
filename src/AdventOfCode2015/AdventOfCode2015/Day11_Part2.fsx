319509ee83c25ff8f3fea569f748497059e0ff14
Yan Cui <theburningmonk@gmail.com>
2015-12-15 01:44:25 +0000

    - added Day 11 part 1 and 2

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day11_Part2.fsx
@@ -0,0 +1,54 @@
+﻿let input = "hepxcrrq"
+
+let (<&&>) f g x = f x && g x
+
+let threeIncrLetters (pwd : string) =
+    pwd 
+    |> Seq.map int 
+    |> Seq.windowed 3
+    |> Seq.exists (fun [| a; b; c |] ->
+        b = a + 1 && c = b + 1)
+
+let iol = set [ 'i'; 'o'; 'l' ]
+let noIOL (pwd : string) =
+    pwd |> Seq.forall (iol.Contains >> not)
+
+let atLeastTwoPairs (pwd : string) =
+    pwd 
+    |> Seq.windowed 2
+    |> Seq.filter (fun [| a; b |] -> a = b)
+    |> Seq.distinct
+    |> Seq.length
+    |> fun n -> n >= 2
+
+let isValid = 
+    threeIncrLetters <&&>
+    noIOL <&&>
+    atLeastTwoPairs
+
+let incr (pwd : string) =
+    // 'a' = 97, 'z' = 122
+    let incrChar (c : char) = 
+        match int c + 1 with
+        | n when n > 122 -> 'a'
+        | n -> char n
+
+    let rec incrAt idx (pwd : string) =
+        let pwd =
+            pwd.Substring(0, idx)
+            + string (incrChar pwd.[idx])
+            + pwd.Substring(idx+1)
+        
+        match pwd.[idx] with
+        // char looped around, incr the next char
+        | 'a' -> incrAt (idx-1) pwd
+        | _   -> pwd
+
+    incrAt (pwd.Length-1) pwd
+
+Seq.unfold (fun pwd -> 
+    let nxtPwd = incr pwd
+    Some (nxtPwd, nxtPwd)) input
+|> Seq.filter isValid
+|> Seq.skip 1
+|> Seq.head
