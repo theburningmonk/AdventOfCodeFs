dcdff79b12cb4ce2ad1ee3772c13dc465fd4bbd1
Yan Cui <theburningmonk@gmail.com>
2015-12-15 08:45:36 +0000

    - added Day 12 part 1 and 2

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day12.fsx
@@ -0,0 +1,11 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let input = 
+    __SOURCE_DIRECTORY__ + "\Day12_Input.txt"
+    |> File.ReadAllText
+
+let matches = Regex.Matches(input, "([-0-9]+)")
+matches 
+|> Seq.cast<Match>
+|> Seq.sumBy (fun m -> int m.Value)
---
+++ src/AdventOfCode2015/AdventOfCode2015/Day12_Part2.fsx
@@ -0,0 +1,46 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let input = 
+    __SOURCE_DIRECTORY__ + "\Day12_Input.txt"
+    |> File.ReadAllText
+
+let reds = Regex.Matches(input, ":\"red\"")
+
+// analogy for 'open' & 'close' is such that you start with
+// an open door behind you, you wanna find the matching door
+// in front of you to close (i.e. { and })
+// so every time you walk through another open door, you gotta
+// find the matching door for that to close first, and so on
+let findBoundary startingIdx dir open' close' =
+    let rec loop idx dir acc =
+        if acc = 0 then idx
+        else
+            let idx = idx + dir
+            let char = input.[idx]
+            if char = open' then loop idx dir (acc+1)
+            elif char = close' then loop idx dir (acc-1)
+            else loop idx dir acc
+
+    loop startingIdx dir 1
+
+let findBoundaryL idx = findBoundary idx -1 '}' '{'
+let findBoundaryR idx = findBoundary idx 1 '{' '}'
+
+let bounds = 
+    reds
+    |> Seq.cast<Match>
+    |> Seq.map (fun m -> 
+        let start' = findBoundaryL m.Index
+        let end'   = findBoundaryR m.Index
+
+        start', end')
+    |> Seq.toArray
+
+let matches = Regex.Matches(input, "([-0-9]+)")
+matches 
+|> Seq.cast<Match>
+|> Seq.filter (fun m ->
+    not <| Array.exists (fun (start', end') ->
+        m.Index > start' && m.Index < end') bounds)
+|> Seq.sumBy (fun m -> int m.Value)
