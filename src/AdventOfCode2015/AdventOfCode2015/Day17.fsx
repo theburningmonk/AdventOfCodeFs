1a9aa7a2e595cb51b2c86c208dc90dd23f247b0c
Yan Cui <theburningmonk@gmail.com>
2015-12-19 11:47:26 +0000

    - added first pass for Day 17

---
+++ src/AdventOfCode2015/AdventOfCode2015/Day17.fsx
@@ -0,0 +1,27 @@
+﻿#load "Day17.fs"
+
+open Day17
+
+let sizes = input.Split '\n' |> Seq.map int |> Seq.toList
+let total = 150
+
+let combine (sizes : int list) =
+    let rec loop sizes (subtotal, acc) =
+        seq {
+            match sizes with
+            | [] -> yield acc |> List.filter (snd >> (<>) 0)
+            | size::rest ->
+                let max = (total - subtotal) / size
+                for n in 0..max do
+                    let subtotal' = subtotal + n * size
+                    yield! loop rest (subtotal', (size, n)::acc)
+        }
+    
+    loop sizes (0, [])
+
+combine sizes
+|> Seq.filter (fun combo ->
+    combo 
+    |> List.sumBy (fun (size, n) -> size * n)
+    |> (=) 150)
+|> Seq.length
