df6b50273ab9c7e9a7d5e2e6292299c4535bee41
Yan Cui <theburningmonk@gmail.com>
2015-12-24 21:18:49 +0000

    - finished Day 24 part 1 and 2

--- src/AdventOfCode2015/AdventOfCode2015/Day24_Part2.fsx
+++ src/AdventOfCode2015/AdventOfCode2015/Day24_Part2.fsx
@@ -5,19 +5,19 @@ open Day24
 let weights = input.Split '\n' |> Seq.map uint64 |> Seq.toList
 
 let totalWeight = Seq.sum weights
-let avgWeight   = totalWeight / 3UL
+let avgWeight   = totalWeight / 4UL
 
 let inline quantumEntanglement gr = gr |> Seq.reduce (*)
 
-// since all weights are odd, and the avg weight (516) is even
-// we need even number of packages
-// smallest no. of packages we need is 6
+// since all weights are odd, and the avg weight (387) is odd
+// we need odd number of packages
+// smallest no. of packages we need is 5
 let groups =
     let rec loop (total, acc) rest =
         seq {
-            if total = avgWeight && List.length acc = 6 then 
+            if total = avgWeight && List.length acc = 5 then 
                 yield acc
-            elif List.length acc < 6 then
+            elif List.length acc < 5 then
                 match rest with
                 | [] -> ()
                 | hd::rest -> 
