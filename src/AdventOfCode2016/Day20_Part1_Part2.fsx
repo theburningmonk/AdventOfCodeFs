a0aa4c1e1675d7f9b639f5c6dd37bf43229e1c42
theburningmonk <theburningmonk@gmail.com>
2016-12-20 21:32:44 +0000

    - refactored day 20

--- src/AdventOfCode2016/Day20_Part1_Part2.fsx
+++ src/AdventOfCode2016/Day20_Part1_Part2.fsx
@@ -1,17 +1,26 @@
 open System.IO
+open Checked
 
 let input = 
   File.ReadAllLines(__SOURCE_DIRECTORY__ + "/Day20Input.txt")
   |> Seq.map (fun line -> 
     let [| lo; hi |] = line.Split('-')
-    uint32 lo, uint32 hi)
+    int64 lo, int64 hi)
   |> Seq.sortBy fst
   |> Seq.toList
 
-let part1 = 
-  let rec loop lo hi = function
-    | [] -> hi + 1u
+let findAllowedIPs input = 
+  let rec loop lo hi input = seq {
+    match input with
+    | [] -> ()
     | (lo', hi')::tl ->
-      if lo' <= hi + 1u then loop lo (max hi hi') tl else hi + 1u
-  
-  loop 0u 0u input
+      if lo' > hi + 1L then 
+        yield! { hi+1L .. lo'-1L}
+      
+      yield! loop lo (max hi hi') tl
+  }
+
+  loop 0L 0L input
+
+let part1 = findAllowedIPs input |> Seq.head
+let part2 = findAllowedIPs input |> Seq.length
